318206569c5d953fcad17487e250df22a0e8254e
Fred Morris <FredrickCPMorris@hotmail.com>
2022-09-26 10:23:18 -0500

    Fixed Repository name, due to speling error

---
+++ battlefield.py
@@ -0,0 +1,20 @@
+from dinosaur import Dinosaur
+from robot import Robot
+
+class Battlefield:
+    def __init__(self):
+        self.robot = Robot
+        self.dinosaur = Dinosaur
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        print('\n Greetings, this is a battle between the past and the futute! \n See who will prevail! \n')
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
+
---
+++ dinosaur.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.attack_power = int(attack_power)
+        self.health = 0
+
+    def attack(self, robot):
+        pass
